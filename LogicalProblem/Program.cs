2de78cfbc0f394203eb9d98c073ebdbdeac2c2aa
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-08 21:03:43 +0530

    [Roshan]Added:Stop-Watch

--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -9,7 +9,7 @@ namespace Day6Assignment
         static void Main(string[] args)
         {
             Console.WriteLine("welecome to fobonaci series ");
-            Console.WriteLine("chooes program\n1. fibonaci series\n2.find perfect number\n3.find prime number or not\n4.reverse a number\n5.coupan Number");
+            Console.WriteLine("chooes program\n1. fibonaci series\n2.find perfect number\n3.find prime number or not\n4.reverse a number\n5.coupan Number\n6.stop watch program");
             int opt = Convert.ToInt32(Console.ReadLine());
 
             switch (opt)
@@ -28,6 +28,9 @@ namespace Day6Assignment
                     break;
                 case 5:
                     coupanNumber.Coupon();
+                    break;
+                case 6:
+                    StopWatch.Stopwatch();
                     break;
 
             }
---
+++ LogicalProblem/StopWatch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+    internal class StopWatch
+    {
+        public static void Stopwatch()
+        {
+            int s = 0;
+            int m = 0;
+            int h = 0;
+            for (s = 0; s <= 59; s++)
+            {
+                Console.WriteLine("Second: {0}", s);
+                if (s == 59)
+                {
+                    m = 1;
+                    while (m <= 59)
+                    {
+                        for (s = 0; s <= 59; s++)
+                        {
+                            Console.WriteLine(m + ":" + s);
+
+                        }
+
+                        m++;
+                    }
+                    m = 59;
+                    // Console.WriteLine("Minute: " + m+ ":"+ s);
+                    if (m == 59)
+                    {
+                        h = 1;
+                        while (h <= 24)
+                        {
+                            //Console.WriteLine("Hour :" + h);
+                            m = 1;
+                            while (m <= 59)
+                            {
+                                //Console.WriteLine("Minute :" + m);
+
+                                for (s = 0; s <= 59; s++)
+                                {
+                                    Console.WriteLine(h + ":" + m + ":" + s);
+                                }
+                                m++;
+                            }
+                            h++;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
