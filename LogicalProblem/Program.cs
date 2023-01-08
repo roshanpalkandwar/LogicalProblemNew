d7056ad2c1cca72daf6cdb35816ca5617a05eb28
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-08 20:35:23 +0530

    [Roshan]Added:Create Febonic Series

---
+++ LogicalProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿using LogicalProblem;
+using System.Diagnostics;
+using System;
+
+namespace Day6Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecome to fobonaci series ");
+            Console.WriteLine("chooes program\n1. fibonaci series");
+            int opt = Convert.ToInt32(Console.ReadLine());
+
+            switch (opt)
+            {
+                case 1:
+                    fibonaciSeries.FibonaciSeries();
+                    break;
+                
+            }
+        }
+    }
+}
---
+++ LogicalProblem/fibonaciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+    internal class fibonaciSeries
+    {
+        public static void FibonaciSeries()
+        {
+            int a = 0, b = 1;
+
+
+            Console.WriteLine("enter the number for limit of series");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int c;
+            for (int i = 1; i < n; i++)
+            {
+                c = a + b;
+                Console.WriteLine(+c);
+                a = b;
+                b = c;
+
+
+            }
+
+
+        }
+    }
+}
