8b772c7a57e8d87068e5a5c7713782882608b7b7
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-08 21:38:31 +0530

    [Roshan]Added:Fewest Notes to be returned for Vending Machine

--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -9,7 +9,7 @@ namespace Day6Assignment
         static void Main(string[] args)
         {
             Console.WriteLine("welecome to fobonaci series ");
-            Console.WriteLine("chooes program\n1. fibonaci series\n2.find perfect number\n3.find prime number or not\n4.reverse a number\n5.coupan Number\n6.stop watch program");
+            Console.WriteLine("chooes program\n1. fibonaci series\n2.find perfect number\n3.find prime number or not\n4.reverse a number\n5.coupan Number\n6.stop watch program\n7.Find the Fewest Notes to be returned for Vending Machine");
             int opt = Convert.ToInt32(Console.ReadLine());
 
             switch (opt)
@@ -31,6 +31,9 @@ namespace Day6Assignment
                     break;
                 case 6:
                     StopWatch.Stopwatch();
+                    break;
+                case 7:
+                    vedingMachineproblem.VedingMachineProblem();
                     break;
 
             }
---
+++ LogicalProblem/vedingMachineproblem.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+    internal class vedingMachineproblem
+    {
+        public static void VedingMachineProblem()
+        {
+            int t = 0;
+            int fh = 0;
+            int h = 0;
+            int f = 0;
+            int ten = 0;
+            int fiv = 0;
+            int tw = 0;
+            int one = 0;
+
+            int n;
+            Console.WriteLine("Enter Number you want change of: ");
+            n = int.Parse(Console.ReadLine());
+
+            if (n >= 1000)
+            {
+
+
+
+                while (n >= 1000)
+                {
+                    n = n - 1000;
+                    t++;
+
+                }
+                Console.WriteLine("you got " + t + " Number of 1000.");
+
+                if (n >= 500)
+                {
+                    while (n >= 500)
+                    {
+                        n = n - 500;
+                        fh++;
+                    }
+                    Console.WriteLine("you got " + fh + " Number of 500.");
+
+
+                    if (n >= 100)
+                    {
+                        while (n >= 100)
+                        {
+                            n = n - 100;
+                            h++;
+
+                        }
+                        Console.WriteLine("you got " + h + " Notes of 100.");
+
+                        if (n >= 50)
+                        {
+                            while (n >= 50)
+                            {
+                                n = n - 50;
+                                f++;
+
+                            }
+                            Console.WriteLine("you got " + f + " Notes of 50.");
+
+                            if (n >= 10)
+                            {
+                                while (n >= 10)
+                                {
+                                    n = n - 10;
+                                    ten++;
+                                }
+                                Console.WriteLine("you got " + ten + " Notes of 10.");
+
+                                if (n >= 5)
+                                {
+                                    while (n >= 5)
+                                    {
+                                        n = n - 5;
+                                        fiv++;
+                                    }
+                                    Console.WriteLine("you got " + fiv + " Number of 5 ");
+
+                                    if (n >= 2)
+                                    {
+                                        while (n >= 2)
+                                        {
+                                            n = n - 2;
+                                            tw++;
+                                        }
+                                        Console.WriteLine("you got " + tw + " Number of 2 ");
+
+                                        if (n >= 1)
+                                        {
+                                            while (n >= 1)
+                                            {
+                                                n = n - 1;
+                                                one++;
+                                            }
+                                            Console.WriteLine("you got " + t + " Number of 1 ");
+                                        }
+                                    }
+
+
+                                }
+
+
+
+                            }
+
+
+
+                        }
+
+
+
+                    }
+                }
+            }
+
+            if (n >= 500)
+            {
+                while (n >= 500)
+                {
+                    n = n - 500;
+                    fh++;
+                }
+                Console.WriteLine("you got " + fh + " Number of 500.");
+
+
+                if (n >= 100)
+                {
+                    while (n >= 100)
+                    {
+                        n = n - 100;
+                        h++;
+
+                    }
+                    Console.WriteLine("you got " + h + " Notes of 100.");
+
+                    if (n >= 50)
+                    {
+                        while (n >= 50)
+                        {
+                            n = n - 50;
+                            f++;
+
+                        }
+                        Console.WriteLine("you got " + f + " Notes of 50.");
+
+                        if (n >= 10)
+                        {
+                            while (n >= 10)
+                            {
+                                n = n - 10;
+                                ten++;
+                            }
+                            Console.WriteLine("you got " + ten + " Notes of 10.");
+
+                            if (n >= 5)
+                            {
+                                while (n >= 5)
+                                {
+                                    n = n - 5;
+                                    fiv++;
+                                }
+                                Console.WriteLine("you got " + fiv + " Number of 5 ");
+
+                                if (n >= 2)
+                                {
+                                    while (n >= 2)
+                                    {
+                                        n = n - 2;
+                                        tw++;
+                                    }
+                                    Console.WriteLine("you got " + tw + " Number of 2 ");
+
+                                    if (n >= 1)
+                                    {
+                                        while (n >= 1)
+                                        {
+                                            n = n - 1;
+                                            one++;
+                                        }
+                                        Console.WriteLine("you got " + t + " Number of 1 ");
+
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            if (n >= 100)
+            {
+                while (n >= 100)
+                {
+                    n = n - 100;
+                    h++;
+
+                }
+                Console.WriteLine("you got " + h + " Notes of 100.");
+
+                if (n >= 50)
+                {
+                    while (n >= 50)
+                    {
+                        n = n - 50;
+                        f++;
+
+                    }
+                    Console.WriteLine("you got " + f + " Notes of 50.");
+
+                    if (n >= 10)
+                    {
+                        while (n >= 10)
+                        {
+                            n = n - 10;
+                            ten++;
+                        }
+                        Console.WriteLine("you got " + ten + " Notes of 10.");
+
+                        if (n >= 5)
+                        {
+                            while (n >= 5)
+                            {
+                                n = n - 5;
+                                fiv++;
+                            }
+                            Console.WriteLine("you got " + fiv + " Number of 5 ");
+
+                            if (n >= 2)
+                            {
+                                while (n >= 2)
+                                {
+                                    n = n - 2;
+                                    tw++;
+                                }
+                                Console.WriteLine("you got " + tw + " Number of 2 ");
+
+                                if (n >= 1)
+                                {
+                                    while (n >= 1)
+                                    {
+                                        n = n - 1;
+                                        one++;
+                                    }
+                                    Console.WriteLine("you got " + t + " Number of 1 ");
+
+
+
+
+
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (n >= 50)
+            {
+                while (n >= 50)
+                {
+                    n = n - 50;
+                    f++;
+
+                }
+                Console.WriteLine("you got " + f + " Notes of 50.");
+
+                if (n >= 10)
+                {
+                    while (n >= 10)
+                    {
+                        n = n - 10;
+                        ten++;
+                    }
+                    Console.WriteLine("you got " + ten + " Notes of 10.");
+
+                    if (n >= 5)
+                    {
+                        while (n >= 5)
+                        {
+                            n = n - 5;
+                            fiv++;
+                        }
+                        Console.WriteLine("you got " + fiv + " Number of 5 ");
+
+                        if (n >= 2)
+                        {
+                            while (n >= 2)
+                            {
+                                n = n - 2;
+                                tw++;
+                            }
+                            Console.WriteLine("you got " + tw + " Number of 2 ");
+
+                            if (n >= 1)
+                            {
+                                while (n >= 1)
+                                {
+                                    n = n - 1;
+                                    one++;
+                                }
+                                Console.WriteLine("you got " + t + " Number of 1 ");
+
+
+
+                            }
+
+
+                        }
+                    }
+                }
+            }
+
+
+            if (n >= 10)
+            {
+                while (n >= 10)
+                {
+                    n = n - 10;
+                    ten++;
+                }
+                Console.WriteLine("you got " + ten + " Notes of 10.");
+
+                if (n >= 5)
+                {
+                    while (n >= 5)
+                    {
+                        n = n - 5;
+                        fiv++;
+                    }
+                    Console.WriteLine("you got " + fiv + " Number of 5 ");
+
+                    if (n >= 2)
+                    {
+                        while (n >= 2)
+                        {
+                            n = n - 2;
+                            tw++;
+                        }
+                        Console.WriteLine("you got " + tw + " Number of 2 ");
+
+                        if (n >= 1)
+                        {
+                            while (n >= 1)
+                            {
+                                n = n - 1;
+                                one++;
+                            }
+                            Console.WriteLine("you got " + t + " Number of 1 ");
+
+
+
+                        }
+                    }
+                }
+            }
+
+
+            if (n >= 5)
+            {
+                while (n >= 5)
+                {
+                    n = n - 5;
+                    fiv++;
+                }
+                Console.WriteLine("you got " + fiv + " Number of 5 ");
+
+                if (n >= 2)
+                {
+                    while (n >= 2)
+                    {
+                        n = n - 2;
+                        tw++;
+                    }
+                    Console.WriteLine("you got " + tw + " Number of 2 ");
+
+                    if (n >= 1)
+                    {
+                        while (n >= 1)
+                        {
+                            n = n - 1;
+                            one++;
+                        }
+                        Console.WriteLine("you got " + t + " Number of 1 ");
+
+
+
+                    }
+                }
+            }
+
+            if (n >= 2)
+            {
+                while (n >= 2)
+                {
+                    n = n - 2;
+                    tw++;
+                }
+                Console.WriteLine("you got " + tw + " Number of 2 ");
+
+                if (n >= 1)
+                {
+                    while (n >= 1)
+                    {
+                        n = n - 1;
+                        one++;
+                    }
+                    Console.WriteLine("you got " + t + " Number of 1 ");
+
+
+
+                }
+
+
+            }
+
+            if (n >= 1)
+            {
+                while (n >= 1)
+                {
+                    n = n - 1;
+                    one++;
+                }
+                Console.WriteLine("you got " + t + " Number of 1 ");
+
+
+
+            }
+
+        }
+    }
+}
